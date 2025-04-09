06c91ca5d6472051751eba927441b8888c180e29
Brandon Herrera <157727691+BrandonHerrera23@users.noreply.github.com>
2025-04-09 12:56:38 -0600

    Create helloworld.bat
    Subiendo el script helloworld.bat

---
+++ Hands-on 2/Hands-on 2/helloworld/helloworld.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Hello World
+echo.
+echo Contenido del directorio actual:
+dir
+echo.
+echo Creando subdirectorio Test...
+mkdir Test
+cd Test
+echo Contenido del subdirectorio Test:
+dir
+pause
